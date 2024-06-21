bb2dd0a42740061a0facbd8b4600d83c7770a8e5
Daniel Souza <workdndsza@gmail.com>
2024-06-21 16:38:00 -0300

    adicionando orest framework

--- setup/settings.py
+++ setup/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'escola',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +63,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'setup.wsgi.application'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Database
